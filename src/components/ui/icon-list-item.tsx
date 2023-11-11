de95b73c7c5c153e228093ee37820db76ce0d04d
Abel Van Steenweghen <abel.van.steenweghen@gmail.com>
2023-11-11 15:09:12 +0200

    Goals Page

--- src/components/ui/icon-list-item.tsx
+++ src/components/ui/icon-list-item.tsx
@@ -11,7 +11,7 @@ export function IconListItem({
   text: string;
 }) {
   return (
-    <div className="flex w-full max-w-md items-center gap-2 rounded-xl  bg-gray-100 p-2 pr-3 text-gray-700">
+    <div className="flex w-full items-center gap-2 rounded-xl  bg-gray-100 p-2 pr-3 text-gray-700">
       <IconSquare color={color} icon={icon} />
       <p className="text-lg font-medium">{text}</p>
       <Spacer />
--- src/pages/goals.tsx
+++ src/pages/goals.tsx
@@ -3,20 +3,72 @@ import CardsStats from "../components/Cards/CardStats";
 import React from "react";
 import { TitleBar } from "~/components/ListDetail/TitleBar";
 import GoalCard from "~/components/Cards/GoalCard";
+import { Button } from "~/components/ui/button";
+import { title } from "process";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "~/components/ui/card";
+import { ListItem } from "~/components/ListDetail/ListItem";
+import { SparkleIcon } from "lucide-react";
+import { IconListItem } from "~/components/ui/icon-list-item";
 
 export default function Goals() {
   return (
     <>
-      <TitleBar
-        // titleRef={titleRef}
-        // scrollContainerRef={scrollContainerRef}
-        title="Goals"
-      />
-      <div className="grid grid-cols-2 gap-8 px-8">
-        <GoalCard title="Marathon" subtitle="Wow 40km " />
-        <GoalCard title="Goal 2" subtitle="Subtitle 2" />
-        <GoalCard title="Goal 3" subtitle="Subtitle 3" />
-        <GoalCard title="Goal 4" subtitle="Subtitle 4" />
+      <div className="flex items-center justify-between">
+        <TitleBar
+          // titleRef={titleRef}
+          // scrollContainerRef={scrollContainerRef}
+          title="Goals"
+        />
+        <Button className="mr-4">Create new goal</Button>
+      </div>
+
+      <div className="p-8">
+        <div className="flex items-center gap-8">
+          <Card onClick={() => console.log("Yeet")} className="flex-grow">
+            <CardHeader>
+              <CardTitle>{"Today's Summary"}</CardTitle>
+              <CardDescription>{"You doing good bro"}</CardDescription>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-4">
+              <IconListItem icon={SparkleIcon} text="Activity A" color="red" />
+              <IconListItem
+                icon={SparkleIcon}
+                text="Activity B"
+                color="yellow"
+              />
+              <IconListItem icon={SparkleIcon} text="Activity C" color="pink" />
+            </CardContent>
+          </Card>
+
+          <Card onClick={() => console.log("Yeet")} className="flex-grow">
+            <CardHeader>
+              <CardTitle>{"Tomorrow"}</CardTitle>
+              <CardDescription>{"Get ready boiii"}</CardDescription>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-4">
+              <IconListItem icon={SparkleIcon} text="Activity A" color="red" />
+              <IconListItem
+                icon={SparkleIcon}
+                text="Activity B"
+                color="yellow"
+              />
+              <IconListItem icon={SparkleIcon} text="Activity C" color="pink" />
+            </CardContent>
+          </Card>
+        </div>
+
+        <div className="grid grid-cols-2 gap-8 py-8">
+          <GoalCard title="Marathon" subtitle="Wow 40km " />
+          <GoalCard title="Goal 2" subtitle="Subtitle 2" />
+          <GoalCard title="Goal 3" subtitle="Subtitle 3" />
+          <GoalCard title="Goal 4" subtitle="Subtitle 4" />
+        </div>
       </div>
     </>
   );
